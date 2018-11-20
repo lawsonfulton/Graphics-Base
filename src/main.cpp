2c5f45d42c6dfffdb6eb3e4271e237b9e859930f
zero-impact <lawsonfulton@gmail.com>
2018-11-20 17:47:50 -0500

    ok done for now

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,25 @@ Eigen::MatrixXi F;
 // This function is called every time a keyboard button is pressed
 bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
 {
-  return false;
+	return false;
 }
 
 int main(int argc, char *argv[])
 {
-  using namespace Eigen;
-  using namespace std;
+	using namespace Eigen;
+	using namespace std;
 
-  // Load a surface mesh
-  igl::read_triangle_mesh(argv[1], V, F);
+	// Load a surface mesh
+	igl::read_triangle_mesh(argv[1], V, F);
 
-  // Plot the generated mesh
-  igl::opengl::glfw::Viewer viewer;
+	// Plot the generated mesh
+	igl::opengl::glfw::Viewer viewer;
 
-  viewer.data().clear();
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
+	viewer.data().clear();
+	viewer.data().set_mesh(V, F);
+	viewer.data().set_face_based(true);
 
-  viewer.callback_key_down = &key_down;
-  key_down(viewer,'5',0);
-  viewer.launch();
+	viewer.callback_key_down = &key_down;
+	key_down(viewer,'5',0);
+	viewer.launch();
 }
